7f4e8401c422b2ae7968c70f97c6c3ecbe1d8b5e
Abdurrahim Surtee <113867655+Surtee97@users.noreply.github.com>
2023-05-07 00:37:01 -0500

    Workis
    Updated and fixed errors

--- Project.py
+++ Project.py
@@ -6,7 +6,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
-
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import KMeans
 
 def adjust_brightness(gray_image, lower_bound=0.4, upper_bound=0.6):
     mean_brightness = np.mean(gray_image) / 255
@@ -19,63 +20,59 @@ def adjust_brightness(gray_image, lower_bound=0.4, upper_bound=0.6):
     adjusted_image = cv2.convertScaleAbs(gray_image, alpha=factor)
     return adjusted_image
 
-
 def resize_image(image, size):
     return cv2.resize(image, size, interpolation=cv2.INTER_LINEAR)
-
 
 def extract_sift_features(gray_image):
     sift = cv2.SIFT_create()
     _, descriptors = sift.detectAndCompute(gray_image, None)
     return descriptors
 
-
 def extract_histogram_features(gray_image, num_bins=256):
     hist = cv2.calcHist([gray_image], [0], None, [num_bins], [0, 256])
     hist = cv2.normalize(hist, hist).flatten()
     return hist
 
+def cluster_features(stacked_descriptors, n_clusters):
+    kmeans = MiniBatchKMeans(n_clusters=n_clusters, random_state=42)
+    kmeans.fit(stacked_descriptors)
+    return kmeans
 
 def train_knn_raw_pixel(X_train, y_train):
+    n_samples = len(X_train)
+    X_train_array = np.array(X_train)
+    flattened_X_train = X_train_array.reshape(n_samples, -1)
     knn = KNeighborsClassifier()
-    knn.fit(X_train, y_train)
+    knn.fit(flattened_X_train, y_train)
     return knn
-
 
 def train_knn_sift(X_train, y_train):
     knn = KNeighborsClassifier()
     knn.fit(X_train, y_train)
     return knn
 
-
 def train_knn_histogram(X_train, y_train):
     knn = KNeighborsClassifier()
     knn.fit(X_train, y_train)
     return knn
-
 
 def train_svm_sift(X_train, y_train):
     svm = SVC(kernel='linear')
     svm.fit(X_train, y_train)
     return svm
 
-
 def calculate_accuracy(y_true, y_pred):
     return accuracy_score(y_true, y_pred)
-
 
 def calculate_false_positive_rate(y_true, y_pred):
     cm = confusion_matrix(y_true, y_pred)
     fp = cm.sum(axis=0) - np.diag(cm)
     return fp / np.sum(cm, axis=0)
 
-
 def calculate_false_negative_rate(y_true, y_pred):
     cm = confusion_matrix(y_true, y_pred)
     fn = cm.sum(axis=1) - np.diag(cm)
     return fn / np.sum(cm, axis=1)
-
-
 
 # Load training and testing images
 def load_images_from_folders(train_dir, test_dir):
@@ -105,142 +102,118 @@ test_directory = "C:/Users/Surtee/Desktop/CS 4391/Project/ProjData/Test"
 
 train_images, train_labels, test_images, test_labels = load_images_from_folders(train_directory, test_directory)
 
-
 # Preprocess images
-def preprocess_images(images):
-    preprocessed_images = []
-    for img in images:
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        adjusted_img = adjust_brightness(gray_img)
-        resized_img_200 = resize_image(adjusted_img, (200, 200))
-        resized_img_50 = resize_image(adjusted_img, (50, 50))
-        preprocessed_images.append((resized_img_200, resized_img_50))
-    return preprocessed_images
+train_gray_images = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in train_images]
+test_gray_images = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in test_images]
 
-train_images_preprocessed = preprocess_images(train_images)
-test_images_preprocessed = preprocess_images(test_images)
+train_gray_images = [adjust_brightness(img) for img in train_gray_images]
+test_gray_images = [adjust_brightness(img) for img in test_gray_images]
 
+# Resize images
+train_resized_images = [resize_image(img, (32, 32)) for img in train_gray_images]
+test_resized_images = [resize_image(img, (32, 32)) for img in test_gray_images]
 
-# Organize training and testing sets
-def raw_pixel_values(images):
-    raw_pixels = []
-    for resized_img_200, resized_img_50 in images:
-        raw_pixels.append(resized_img_50.flatten())
-    return np.array(raw_pixels)
+# Extract features
+train_sift_features = [extract_sift_features(img) for img in train_gray_images]
+test_sift_features = [extract_sift_features(img) for img in test_gray_images]
 
-X_train_raw_pixels = raw_pixel_values(train_images_preprocessed)
-X_test_raw_pixels = raw_pixel_values(test_images_preprocessed)
+train_histogram_features = [extract_histogram_features(gray_img) for gray_img in train_resized_images]
+test_histogram_features = [extract_histogram_features(gray_img) for gray_img in test_resized_images]
 
-def sift_features(images):
-    sift_feats = []
-    for resized_img_200, resized_img_50 in images:
-        sift_feat = extract_sift_features(resized_img_200)
-        sift_feats.append(sift_feat)
-    return sift_feats
+train_gray_images, train_sift_features, train_labels = zip(*[(img, sift, lbl) for img, sift, lbl in zip(train_gray_images, train_sift_features, train_labels) if sift is not None])
+test_gray_images, test_sift_features, test_labels = zip(*[(img, sift, lbl) for img, sift, lbl in zip(test_gray_images, test_sift_features, test_labels) if sift is not None])
 
-X_train_sift = sift_features(train_images_preprocessed)
-X_test_sift = sift_features(test_images_preprocessed)
-
-def histogram_features(images):
-    hist_feats = []
-    for resized_img_200, resized_img_50 in images:
-        hist_feat = extract_histogram_features(resized_img_200)
-        hist_feats.append(hist_feat)
-    return np.array(hist_feats)
-
-X_train_histogram = histogram_features(train_images_preprocessed)
-X_test_histogram = histogram_features(test_images_preprocessed)
-
+# Convert labels to lowercase
+train_labels = [label.lower() for label in train_labels]
+test_labels = [label.lower() for label in test_labels]
 
 # Encode the labels
 label_encoder = LabelEncoder()
 y_train_encoded = label_encoder.fit_transform(train_labels)
 y_test_encoded = label_encoder.transform(test_labels)
 
-# Train classifiers, Test classifiers, and report results
-# Train Nearest Neighbor classifier
-knn_raw_pixel = train_knn_raw_pixel(X_train_raw_pixels, y_train_encoded)
+# Stack all SIFT descriptors
+def stack_descriptors(train_sift_features):
+    stacked_descriptors = []
+    for descriptors in train_sift_features:
+        for desc in descriptors:
+            stacked_descriptors.append(desc)
+    return stacked_descriptors
 
-# Make predictions on the test set
-y_pred_raw_pixel = knn_raw_pixel.predict(X_test_raw_pixels)
+stacked_descriptors = stack_descriptors(train_sift_features)
 
-# Evaluate the classifier
-accuracy_raw_pixel = calculate_accuracy(y_test_encoded, y_pred_raw_pixel)
-fp_rate_raw_pixel = calculate_false_positive_rate(y_test_encoded, y_pred_raw_pixel)
-fn_rate_raw_pixel = calculate_false_negative_rate(y_test_encoded, y_pred_raw_pixel)
+# Cluster SIFT features
+def cluster_features(train_descriptors, n_clusters):
+    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans.fit(train_descriptors)
+    return kmeans
 
-print("Raw pixel values:")
-print(f"Accuracy: {accuracy_raw_pixel * 100:.2f}%")
-print(f"False positive rate: {fp_rate_raw_pixel}")
-print(f"False negative rate: {fn_rate_raw_pixel}")
+n_clusters = 50
+train_descriptors = np.vstack(train_sift_features)
+kmeans = cluster_features(train_descriptors, n_clusters)
 
-# Modify the training and evaluation functions to handle lists of descriptors
-# Train Nearest Neighbor classifier
-knn_sift = train_knn_sift(X_train_sift, y_train_encoded)
+# Calculate BoW histograms
+train_histograms = []
+for features in train_sift_features:
+    histogram = np.zeros(n_clusters)
+    if features is not None:
+        cluster_assignments = kmeans.predict(features)
+        for assignment in cluster_assignments:
+            histogram[assignment] += 1
+    train_histograms.append(histogram)
 
-# Make predictions on the test set
-y_pred_sift = knn_sift.predict(X_test_sift)
+test_histograms = []
+for features in test_sift_features:
+    histogram = np.zeros(n_clusters)
+    if features is not None:
+        cluster_assignments = kmeans.predict(features)
+        for assignment in cluster_assignments:
+            histogram[assignment] += 1
+    test_histograms.append(histogram)
 
-# Evaluate the classifier
-accuracy_sift = calculate_accuracy(y_test_encoded, y_pred_sift)
-fp_rate_sift = calculate_false_positive_rate(y_test_encoded, y_pred_sift)
-fn_rate_sift = calculate_false_negative_rate(y_test_encoded, y_pred_sift)
+# Train and test classifiers
+knn_raw_pixel = train_knn_raw_pixel(train_resized_images, y_train_encoded)
+knn_sift = train_knn_sift(train_histograms, y_train_encoded)
+knn_histogram = train_knn_histogram(train_histogram_features, y_train_encoded)
+svm_sift = train_svm_sift(train_histograms, y_train_encoded)
 
-print("SIFT features:")
-print(f"Accuracy: {accuracy_sift * 100:.2f}%")
-print(f"False positive rate: {fp_rate_sift}")
-print(f"False negative rate: {fn_rate_sift}")
+# Reshape test images for KNN raw pixel classifier
+n_test_samples = len(test_resized_images)
+test_resized_images_array = np.array(test_resized_images)
+flattened_test_resized_images = test_resized_images_array.reshape(n_test_samples, -1)
 
-# Train Nearest Neighbor classifier
-knn_histogram = train_knn_histogram(X_train_histogram, y_train_encoded)
+# Predict test data
+y_pred_knn_raw_pixel = knn_raw_pixel.predict(flattened_test_resized_images)
+y_pred_knn_sift = knn_sift.predict(test_histograms)
+y_pred_knn_histogram = knn_histogram.predict(test_histogram_features)
+y_pred_svm_sift = svm_sift.predict(test_histograms)
 
-# Make predictions on the test set
-y_pred_histogram = knn_histogram.predict(X_test_histogram)
+accuracy_knn_raw_pixel = calculate_accuracy(y_test_encoded, y_pred_knn_raw_pixel)
+fp_rate_raw_pixel = calculate_false_positive_rate(y_test_encoded, y_pred_knn_raw_pixel)
+fn_rate_raw_pixel = calculate_false_negative_rate(y_test_encoded, y_pred_knn_raw_pixel)
 
-# Evaluate the classifier
-accuracy_histogram = calculate_accuracy(y_test_encoded, y_pred_histogram)
-fp_rate_histogram = calculate_false_positive_rate(y_test_encoded, y_pred_histogram)
-fn_rate_histogram = calculate_false_negative_rate(y_test_encoded, y_pred_histogram)
+accuracy_knn_sift = calculate_accuracy(y_test_encoded, y_pred_knn_sift)
+fp_rate_sift = calculate_false_positive_rate(y_test_encoded, y_pred_knn_sift)
+fn_rate_sift = calculate_false_negative_rate(y_test_encoded, y_pred_knn_sift)
 
-print("Histogram features:")
-print(f"Accuracy: {accuracy_histogram * 100:.2f}%")
-print(f"False positive rate: {fp_rate_histogram}")
-print(f"False negative rate: {fn_rate_histogram}")
+accuracy_knn_histogram = calculate_accuracy(y_test_encoded, y_pred_knn_histogram)
+fp_rate_histogram = calculate_false_positive_rate(y_test_encoded, y_pred_knn_histogram)
+fn_rate_histogram = calculate_false_negative_rate(y_test_encoded, y_pred_knn_histogram)
 
-# Modify the training and evaluation functions to handle lists of descriptors
-# Train Linear SVM classifier
-svm_sift = train_svm_sift(X_train_sift, y_train_encoded)
+accuracy_svm_sift = calculate_accuracy(y_test_encoded, y_pred_svm_sift)
+fp_rate_sift_svm = calculate_false_positive_rate(y_test_encoded, y_pred_svm_sift)
+fn_rate_sift_svm = calculate_false_negative_rate(y_test_encoded, y_pred_svm_sift)
 
-# Make predictions on the test set
-y_pred_sift_svm = svm_sift.predict(X_test_sift)
 
-# Evaluate the classifier
-accuracy_sift_svm = calculate_accuracy(y_test_encoded, y_pred_sift_svm)
-fp_rate_sift_svm = calculate_false_positive_rate(y_test_encoded, y_pred_sift_svm)
-fn_rate_sift_svm = calculate_false_negative_rate(y_test_encoded, y_pred_sift_svm)
-
-print("SIFT features with Linear SVM:")
-print(f"Accuracy: {accuracy_sift_svm * 100:.2f}%")
-print(f"False positive rate: {fp_rate_sift_svm}")
-print(f"False negative rate: {fn_rate_sift_svm}")
-
-print("Summary:")
-print("1. Raw pixel values with Nearest Neighbor:")
-print(f"  Accuracy: {accuracy_raw_pixel * 100:.2f}%")
+print(f"Accuracy KNN Raw Pixel: , {accuracy_knn_raw_pixel* 100:.2f}%")
 print(f"  False positive rate: {fp_rate_raw_pixel}")
 print(f"  False negative rate: {fn_rate_raw_pixel}")
-
-print("2. SIFT features with Nearest Neighbor:")
-print(f"  Accuracy: {accuracy_sift * 100:.2f}%")
+print(f"Accuracy KNN SIFT: , {accuracy_knn_sift* 100:.2f}%")
 print(f"  False positive rate: {fp_rate_sift}")
 print(f"  False negative rate: {fn_rate_sift}")
-
-print("3. Histogram features with Nearest Neighbor:")
-print(f"  Accuracy: {accuracy_histogram * 100:.2f}%")
+print(f"Accuracy KNN Histogram: , {accuracy_knn_histogram* 100:.2f}%")
 print(f"  False positive rate: {fp_rate_histogram}")
 print(f"  False negative rate: {fn_rate_histogram}")
-
-print("4. SIFT features with Linear SVM:")
-print(f"  Accuracy: {accuracy_sift_svm * 100:.2f}%")
+print(f"Accuracy SVM SIFT: , {accuracy_svm_sift* 100:.2f}%")
 print(f"  False positive rate: {fp_rate_sift_svm}")
 print(f"  False negative rate: {fn_rate_sift_svm}")
